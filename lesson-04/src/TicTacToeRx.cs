8d36820fc07637412d9167ee70c7fde9d0c459be
Henning Torsteinsen <henning.torsteinsen@bouvet.no>
2021-05-04 07:20:54 +0200

    Rx version: Added FirstPlayerIsPlayerX

---
+++ lesson-04/src/TicTacToeRx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reactive.Subjects;
+using System.Reactive.Linq;
+
+namespace src
+{
+    public class TicTacToeRx
+    {
+        public Subject<Player> CurrentPlayer { get; internal set; }
+
+        public Subject<Request> Messages { get; internal set; }
+
+        public TicTacToeRx()
+        {
+            CurrentPlayer = new Subject<Player>();
+            Messages = new Subject<Request>();
+
+            Messages.Subscribe(MessageHandler);
+        }
+
+        private void MessageHandler(Request request)
+        {
+            CurrentPlayer.OnNext(Player.X);
+        }
+
+    }
+
+    public enum Request
+    {
+        CurrentPlayer
+    }
+}
---
+++ lesson-04/test/TicTacToeRxShould.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using src;
+using System;
+using System.Reactive.Linq;
+
+namespace test
+{
+    [TestFixture]
+    public class TicTacToeRxShould
+    {
+        private TicTacToeRx _ticTacToeRx;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _ticTacToeRx = new TicTacToeRx();
+        }
+
+        [Test]
+        public void FirstPlayerIsPlayerX()
+        {
+            Player currentPlayer = Player.None;
+            _ticTacToeRx.CurrentPlayer.Subscribe(player => currentPlayer = player);
+            
+            _ticTacToeRx.Messages.OnNext(Request.CurrentPlayer);
+
+
+            Assert.AreEqual(Player.X, currentPlayer);
+        }
+    }
+}
